d538e0e4cb210e76ff0b3f484cc36acb62d5bb66
MulamuMammba <mammba001@gmail.com>
2025-02-12 09:05:11 +0200

    Added methods for addition and multiplication

--- src/main/java/org/mammba/Calculator.java
+++ src/main/java/org/mammba/Calculator.java
@@ -2,4 +2,18 @@ package org.mammba;
 
 public class Calculator {
 
+    public int add(double... numbers){
+        double sum = 0;
+        for(double number : numbers){
+            sum+=number;
+        }
+        return (int) sum;
+    }
+    public int multiply(double... numbers){
+        double sum = 0;
+        for(double number : numbers){
+            sum*=number;
+        }
+        return (int) sum;
+    }
 }
